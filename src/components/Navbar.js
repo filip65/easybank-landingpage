fdf8f5dc839f384202a3e395fac21734a4dd55d8
Filip Katusin <filip.katusin@gmail.com>
2021-06-16 11:55:05 +0200

    zalohovanie pre upravami

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../css/navbar.css";
 
 import logo from "../images/logo.svg";
 import hamburger from "../images/icon-hamburger.svg";
+import closeIcon from "../images/icon-close.svg";
 
 const Navbar = () => {
   const navbarRef = useRef(null);
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
-  const toggleNavbar = (e) => {
-    if (navbarRef.current.classList.contains("mobile-navbar-opened")) {
-      navbarRef.current.classList.remove("mobile-navbar-opened");
-      navbarRef.current.classList.add("mobile-navbar-closed");
-    } else {
-      navbarRef.current.classList.add("mobile-navbar-opened");
-      navbarRef.current.classList.remove("mobile-navbar-closed");
-    }
+  const openNavbar = () => {
+    navbarRef.current.classList.add("mobile-navbar-opened");
+    navbarRef.current.classList.remove("mobile-navbar-closed");
+    setIsNavbarOpen(true);
+  };
+
+  const closeNavbar = () => {
+    navbarRef.current.classList.remove("mobile-navbar-opened");
+    navbarRef.current.classList.add("mobile-navbar-closed");
+    setIsNavbarOpen(false);
   };
 
   return (
     <div className="navbar">
       <div className="navbar-wraper">
         <img src={logo} alt="logo" />
-        <button className="toggle-btn" onClick={toggleNavbar}>
+
+        <button className="toggle-btn toggle-btn-open" onClick={openNavbar}>
           <img src={hamburger} alt="navbar toggle icon" />
+        </button>
+
+        <button className="toggle-btn toggle-btn-close" onClick={closeNavbar}>
+          <img src={closeIcon} alt="navbar toggle icon" />
         </button>
       </div>
       <div
