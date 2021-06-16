20928298ce74cf241e43690a06ce8e1af103fd20
Filip Katusin <filip.katusin@gmail.com>
2021-06-16 14:48:13 +0200

    dokoncenie navbaru pre mobil

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,69 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "../css/navbar.css";
 
 import logo from "../images/logo.svg";
-import hamburger from "../images/icon-hamburger.svg";
+import hamburgerIcon from "../images/icon-hamburger.svg";
 import closeIcon from "../images/icon-close.svg";
 
 const Navbar = () => {
   const navbarRef = useRef(null);
-  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const openBtn = useRef(null);
+  const closeBtn = useRef(null);
+  const mobileNavbar = useRef(null);
 
-  const openNavbar = () => {
-    navbarRef.current.classList.add("mobile-navbar-opened");
-    navbarRef.current.classList.remove("mobile-navbar-closed");
-    setIsNavbarOpen(true);
+  const [isNavbarOpened, setIsNavbarOpened] = useState(false);
+
+  const toggleNavbar = () => {
+    let state = navbarRef.current.classList.contains("navbar-closed");
+
+    if (state) {
+      navbarRef.current.classList.remove("navbar-closed");
+      navbarRef.current.classList.add("navbar-opened");
+      setIsNavbarOpened(true);
+    } else {
+      navbarRef.current.classList.add("navbar-closed");
+      navbarRef.current.classList.remove("navbar-opened");
+      setIsNavbarOpened(false);
+    }
   };
 
-  const closeNavbar = () => {
-    navbarRef.current.classList.remove("mobile-navbar-opened");
-    navbarRef.current.classList.add("mobile-navbar-closed");
-    setIsNavbarOpen(false);
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      if (isNavbarOpened) {
+        openBtn.current.style.display = "none";
+        closeBtn.current.style.display = "block";
+      } else {
+        closeBtn.current.style.display = "none";
+        openBtn.current.style.display = "block";
+      }
+    }, 500);
+
+    setTimeout(() => {
+      mobileNavbar.current.style.display = "none";
+    }, 1500);
+  }, [isNavbarOpened]);
 
   return (
-    <div className="navbar">
-      <div className="navbar-wraper">
+    <div className="navbar navbar-closed" ref={navbarRef}>
+      <div className="navbar-wrapper">
         <img src={logo} alt="logo" />
-
-        <button className="toggle-btn toggle-btn-open" onClick={openNavbar}>
-          <img src={hamburger} alt="navbar toggle icon" />
+        <button
+          className="toggle-btn toggle-btn-open"
+          onClick={toggleNavbar}
+          ref={openBtn}
+        >
+          <img src={hamburgerIcon} alt="" />
         </button>
-
-        <button className="toggle-btn toggle-btn-close" onClick={closeNavbar}>
-          <img src={closeIcon} alt="navbar toggle icon" />
+        <button
+          className="toggle-btn toggle-btn-close"
+          onClick={toggleNavbar}
+          ref={closeBtn}
+        >
+          <img src={closeIcon} alt="" />
         </button>
       </div>
-      <div
-        className="mobile-navbar mobile mobile-navbar-closed"
-        ref={navbarRef}
-      >
+
+      <div className="mobile-navbar" ref={mobileNavbar}>
         <ul>
           <li>
             <a href="/#">Home</a>
